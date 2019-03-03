f6d9300db76ab3827fe9ae0a005118542c0feeaf
Nagasaki Naoto <kdkrniyuni@gmail.com>
2019-03-03 16:11:45 +0900

    名前変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title><a href="{{ route('home') }}">{{ config('app.name', 'idea-katati') }}</a></title>
+    <title><a href="{{ route('home') }}">{{ config('app.name', 'Spira') }}</a></title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
